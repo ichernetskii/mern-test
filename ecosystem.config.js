660e67af4a44c7a15d9475e9b986bf2c2ccc03b0
Ilya Chernetskiy <info@smarto.pro>
2021-01-17 00:32:14 +0300

    node-env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,6 @@ module.exports = {
         script: "npm -- start",
         watch: ["config", "middleware", "models", "routes", "app.js"],
         ignore_watch: ["node_modules", "client"],
-        watch_delay: 1000,
-        env: {
-            NODE_ENV: 'development',
-        },
-        env_production: {
-            NODE_ENV: 'production',
-        }
+        watch_delay: 1000
     }]
 }
