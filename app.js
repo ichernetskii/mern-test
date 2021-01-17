8c44b16b062e9187d25810c8d4a9adb9c8dcbe69
Ilya Chernetskiy <info@smarto.pro>
2021-01-17 19:12:51 +0300

    ports for nginx

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use("/api/auth", authRouter);
 app.use("/api/link", linkRouter);
 app.use("/t", redirectRouter);
 
-if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static(path.join(__dirname, "client", "build")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use("/", express.static(path.join(__dirname, "client", "build")));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     })
+// }
 
 // MONGOOSE
 const mongoose = require("mongoose");
