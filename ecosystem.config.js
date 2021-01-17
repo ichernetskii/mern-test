3cabebfc3dd3645160f8696a519d2314ee09a351
Ilya Chernetskiy <info@smarto.pro>
2021-01-18 02:10:48 +0300

    env added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,15 @@ module.exports = {
     apps: [{
         name: "mern-test",
         script: "npm -- start",
-        error_file: 'err.log',
-        out_file: 'out.log',
-        log_file: 'combined.log',
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production : {
+            NODE_ENV: "production"
+        },
+        error_file: "err.log",
+        out_file: "out.log",
+        log_file: "combined.log",
         time: true,
         watch: ["config", "middleware", "models", "routes", "app.js"],
         ignore_watch: ["node_modules", "client"],
