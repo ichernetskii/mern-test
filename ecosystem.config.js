4f659f5196dde6da7cdcd2c2571e5d18140d4cfa
Ilya Chernetskiy <info@smarto.pro>
2021-01-17 20:21:22 +0300

    log pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,10 @@ module.exports = {
     apps: [{
         name: "mern-test",
         script: "npm -- start",
+        error_file: 'err.log',
+        out_file: 'out.log',
+        log_file: 'combined.log',
+        time: true,
         watch: ["config", "middleware", "models", "routes", "app.js"],
         ignore_watch: ["node_modules", "client"],
         watch_delay: 1000
